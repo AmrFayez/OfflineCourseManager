04b8c7246dad85ad0102d1b7e9ad8ae545000758
AmrFayez <34596347+AmrFayez@users.noreply.github.com>
2018-05-03 14:44:44 +0200

    Initial

---
+++ OfflineCourseManager.Core/Clip.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineCourseManager.Core
+{
+    class Clip
+    {
+
+        public int Id { get; set; }
+        public bool  IsStarted { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Name { get; set; }
+        public string Thumbnail { get; set; }
+        public FileInfo FileInfo { get; set; }
+        public Clip(int id,FileInfo fileInfo)
+        {
+            FileInfo = fileInfo;
+            Id = id;
+            Initalize();
+        }
+
+        private void Initalize()
+        {
+            Name = FileInfo.Name;
+        }
+    }
+}
---
+++ OfflineCourseManager.Core/Course.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineCourseManager.Core
+{
+    class Course
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Module> Modules { get; set; }
+        public bool IsFinished { get; set; }
+        public bool IsStarted { get; set; }
+        public DirectoryInfo DirectoryInfo { get; set; }
+
+        public List<Course> GetCourses(string filePath)
+        {
+            return null;
+        }
+
+        public Course(string filePath)
+        {
+
+        }
+    }
+}
